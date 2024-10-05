d94bbeb03b3424803cf19c49d5f6980135005a83
Вика <ruseckaya@cmit.ru>
2024-10-05 21:37:02 +0300

    add Monolog in crm-sdk.php

--- crm-sdk.php
+++ crm-sdk.php
@@ -12,10 +12,36 @@ declare(strict_types=1);
 
 use Bitrix24\SDK\Services\ServiceBuilderFactory;
 
-require_once 'vendor/autoload.php';
+$webhookUrl = 'https://b24-kjhrrq.bitrix24.by/rest/1/s1cqjhhnj02fvek5/';
+
+require_once './vendor/autoload.php';
+
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Level;
+use Monolog\Logger;
+use Monolog\Processor\MemoryUsageProcessor;
+use Monolog\Processor\UidProcessor;
+
+// rotating
+$rotatingFileHandler = new RotatingFileHandler( 'b24-crm-scope-debug', 7, Level::Debug);
+$rotatingFileHandler->setFilenameFormat('/logs/'.'{filename}-{date}.log', 'Y-m-d');
+
+$logger = new Logger('crm-sdk');
+$logger->pushHandler($rotatingFileHandler);
+$logger->pushProcessor(new MemoryUsageProcessor(true, true));
+$logger->pushProcessor(new UidProcessor());
+
+// add records to the log
+//$logger->debug("test");
+//$logger->info('Get contacts', ['contacts' => 'all']);
+$logger->error('Error',["Запрос"=>"Не удалось получить..."]);
+$logger->warning('Warning');
 
 /***** CRM */
-$b24Service = ServiceBuilderFactory::createServiceBuilderFromWebhook('https://b24-kjhrrq.bitrix24.by/rest/1/s1cqjhhnj02fvek5/');
+$b24Service = ServiceBuilderFactory::createServiceBuilderFromWebhook(
+    webhookUrl: $webhookUrl,
+    logger: $logger
+);
 
 /** Получить контакты */
 var_dump($b24Service->getCRMScope()->contact()->list([], [], [], 1)->getContacts());
